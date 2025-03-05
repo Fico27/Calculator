d4cbaff53f80209c290b5c1d07b00afce451c65b
Phil <182430206+Fico27@users.noreply.github.com>
2025-03-05 15:28:52 +0100

    Added in new function for operations to get first number(firstUserInput) and the operation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 let firstUserInput = 0;
 let secondUserInput = 0;
-let operation;
+let operation = '';
 
 const screen = document.querySelector(".screen");
 
@@ -18,14 +18,17 @@ function multiply(firstNum, secondNum) {
     return firstNum * secondNum;
 }
 function divide(firstNum, secondNum) {
-    return firstNum / secondNum;
+    if (secondNum === 0){
+        return "The void waits for you"
+    } else return firstNum / secondNum;
+    
 }
 
 
 function operate(num1, num2, operator) {
     let result;
     if(operator === "+"){
-      result =  add(num1, num2)
+        result =  add(num1, num2)
     } else if(operator === "-"){
         result = subtract(num1, num2)
     } else if(operator === "*"){
@@ -38,6 +41,20 @@ function operate(num1, num2, operator) {
 
 function addToScreen(value){
     screen.innerHTML += value
+}
+
+function addToScreenOperation(value){
+    firstUserInput = parseInt(screen.innerHTML)
+    operation = value;
+
+    const operators = ["+", "-", "*", "/"];
+    const lastChar = screen.innerHTML.slice(-1);
+
+    if (operators.includes(lastChar)) {
+        screen.innerHTML = screen.innerHTML.slice(0, -1);
+    } 
+
+    screen.innerHTML += value;
 }
 
 function deleteScreen() {
