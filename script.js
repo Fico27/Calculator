b06b546cd2e8b31405afcc76a2c16a49574f5579
Phil <182430206+Fico27@users.noreply.github.com>
2025-03-05 13:01:53 +0100

    Adding in onClick to buttons to populate the screen. Added in logic to change innerHTML of screen.

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ let firstUserInput = 0;
 let secondUserInput = 0;
 let operation;
 
-
+const screen = document.querySelector(".screen");
 
 
 function add(firstNum, secondNum) {
-    return firstNum + secondNum
+    return firstNum + secondNum;
 }
 
 function subtract(firstNum, secondNum) {
-    return firstNum - secondNum
+    return firstNum - secondNum;
 }
 
 function multiply(firstNum, secondNum) {
-    return firstNum * secondNum
+    return firstNum * secondNum;
 }
 function divide(firstNum, secondNum) {
-    return firstNum / secondNum
+    return firstNum / secondNum;
 }
 
 
@@ -33,8 +33,10 @@ function operate(num1, num2, operator) {
     }else if (operator === "/"){
         result =  divide(num1, num2)
     }
-    return result
+    return result;
 }
 
-
+function addToScreen(value){
+    screen.innerHTML += value
+}
 
